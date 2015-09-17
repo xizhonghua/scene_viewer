e4cc5f282ce3f0f0fe53dc602f05137ae74049ea
Zhonghua Xi <xizhonghua@gmail.com>
2015-09-17 11:45:09 -0400

    support load objs and set position and rotations

--- js/SceneLoader.js
+++ js/SceneLoader.js
@@ -23,19 +23,93 @@ MASC.SceneLoader = function( manager ) {
 
   this.manager = ( manager !== undefined ) ? manager : THREE.DefaultLoadingManager;
 
+  this.models_to_load = 0;
+  this.models_loaded = 0;
+
+  this.models_requested =  {};
+  this.models_cached = {};
+  this.models_queue = {};
 };
 
 MASC.SceneLoader.prototype = {
   constructor: MASC.SceneLoader,
 
+ 
+
+  onObjectProgress : function ( xhr ) {
+      if ( xhr.lengthComputable ) {
+        var percentComplete = xhr.loaded / xhr.total * 100;
+        console.log( Math.round(percentComplete, 2) + '% downloaded' );
+      }
+  },
+
+  onObjLoaded : function( object, path, args ) {
+    // put into caches
+    this.models_cached[path] = object;
+
+    var geometry = object.children[0].geometry;
+
+    // geometry.center ();
+    geometry.computeBoundingSphere();
+    var center = geometry.boundingSphere.center;
+    var scale = 1.0 / geometry.boundingSphere.radius;
+    geometry.translate(-center.x, -center.y, -center.z);
+    geometry.scale(scale, scale, scale);
+    geometry.computeVertexNormals();
+
+
+    // loop all queues
+    var queue = this.models_queue[path];
+    for(var i=0;i<queue.length;++i) {
+      this.addObj(object.clone(), queue[i]);      
+    }
+  },
+
+  addObj : function(object, args) {
+
+    var euler_angle = new THREE.Euler( 0, 0, 0, 'XYZ' );
+
+    for(var i=0;i<args.length;++i) {
+      var kv = args[i].split('=');
+      if(kv.length==1) continue;
+      var key = kv[0];
+      var val = kv[1];
+      if(key == "tx") {
+        object.position.x = parseFloat(val);
+      } else if(key == "ty") {
+        object.position.y = parseFloat(val);
+      } else if(key == "tz") {
+        object.position.z = parseFloat(val);
+      } else if(key == "scale") {
+        object.scale.x = object.scale.y = object.scale.z = parseFloat(val);
+      } else if(key == "rx") {
+        euler_angle.x = parseFloat(val);
+      } else if(key == "ry") {
+        euler_angle.y = parseFloat(val);
+      } else if(key == "rz") {
+        euler_angle.z = parseFloat(val);
+      }
+    }
+
+    object.rotation.copy( euler_angle );
+
+    //TODO(zxi) set properties
+    this.scene.add( object );
+    this.models_loaded ++;
+    console.log('total models added = ' + this.models_loaded);
+  },
+
+  onObjectError : function ( xhr ) { },  
+
   load: function( url, scene, onLoad, onProgress, onError ) {
     var scope = this;
-    this.scene = scene;
+    scope.scene = scene;
+    scope.onLoadCallback = onLoad;
 
     var loader = new THREE.XHRLoader( scope.manager );
     loader.load( url, function ( text ) {
 
-      onLoad( scope.parse( text ) );
+      scope.parse( text );
 
     }, onProgress, onError );
   },
@@ -48,8 +122,43 @@ MASC.SceneLoader.prototype = {
     console.log('lightHandler');
   },
 
-  meshHandler: function( args ) {
-    console.log('meshHandler');
+  meshHandler: function( args ) {    
+
+    var path = args[0];
+    var scope = this;
+
+    console.log('meshHandler path = ' + path);
+
+    args.shift();
+
+    scope.models_to_load ++;
+
+    if (!(path in scope.models_queue)) {
+      scope.models_queue[path] = [];
+    }
+
+    scope.models_queue[path].push(args);    
+
+    if (scope.models_requested[path])
+    {
+      var cached_obj = scope.models_cached[path];
+      if(cached_obj) {
+        scope.addObj(cached_obj.clone(), args);
+      }
+    }
+    else {
+
+      scope.models_requested[path] = true;
+
+      var loader = new THREE.OBJLoader( scope.manager );
+      loader.load( path, 
+        function(object) {
+          scope.onObjLoaded(object, path, args);
+        },
+        scope.onObjProgress, 
+        scope.onObjError );
+    }
+
   },
 
   bboxHandler: function( args ) {
@@ -86,7 +195,7 @@ MASC.SceneLoader.prototype = {
         continue;
       }      
 
-      handler(items);
+      handler.call(this, items);
     }
 
     console.time( 'SceneLoader' );
